644858b4ee701528452dbe1b9cad8c71e387014a
dobrevd <dobrev@ukr.net>
2025-05-19 13:22:14 +0200

    initial commit

---
+++ src/test/java/com/dobrev/urlstatsservice/UrlStatsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobrev.urlstatsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlStatsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
