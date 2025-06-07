e56ab58d58bde46bed72c5f6a2d4102e1e5aa260
dobrevd <dobrev@ukr.net>
2025-06-07 11:28:47 +0200

    add CorsConfig

--- src/main/java/com/dobrev/urlstatsservice/controller/UrlEventsController.java
+++ src/main/java/com/dobrev/urlstatsservice/controller/UrlEventsController.java
@@ -6,10 +6,10 @@ import com.dobrev.urlstatsservice.service.UrlEventsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,8 +27,8 @@ public class UrlEventsController {
         log.info("Event is saved {}", event.toString());
     }
 
-    @GetMapping("/{id}")
-    public List<UrlStatsDto> getByUserId(@PathVariable("id") String userId) {
+    @GetMapping
+    public List<UrlStatsDto> getByUserId(@RequestParam("userId") String userId) {
         return urlEventsService.findByUserId(userId);
     }
 }
